76ba2f7b27273730145517122d87a218098c5dd2
Inu Enike George <105659884+Ennygenius@users.noreply.github.com>
2023-08-25 01:03:42 +0100

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,12 +4,12 @@ dotenv.config()
 
 const URI = process.env.URI
 
-const connect = async (req, res) => {
+const connect = async (req, res, next) => {
     try {
         const connectDB = await mongoose.connect(URI)
         console.log(`Database Connected Successfully ${connectDB.connection.host}:${connectDB.connection.port}/${connectDB.connection.name}`);
     } catch (error) {
-        throw new Error('Database connection Failed')
+        next(error)
     }
 
 }
